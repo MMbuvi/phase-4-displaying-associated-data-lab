0c0197054a3e0a68c4f09478f44186678f377835
Meshack <mbuvi.meshack99@gmail.com>
2022-12-01 08:39:13 +0300

    commit tree 2be94b9b68b3eb4c93c4e08d56173e9ba42cb19b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,35 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all.as_json(include: :user)
+        render json: items
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class DogHousesController < ApplicationController
+#     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+#     def show
+#       dog_house = DogHouse.find(params[:id])
+#       render json: dog_house, include: :reviews
+#     end
+  
+#     private
+  
+#     def render_not_found_response
+#       render json: { error: "Dog house not found" }, status: :not_found
+#     end
+  
+#   end
